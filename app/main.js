a07612ec702e8218674478a960d9b2a0ddab065d
Zachary Cotton <mr.zacharycotton@gmail.com>
2022-04-27 22:58:22 -0700

    Feed from Weather.com

--- app/main.js
+++ app/main.js
@@ -1,28 +1,48 @@
-const express = require('express');
+import express from 'express';
+import fetch from 'node-fetch';
+import { parse } from 'node-html-parser';
 
 const app = express();
 const port = Number.parseInt(process.env.PORT);
 
-app.get('/', (req, res) => {
+const weatherUrl = 'https://weather.com/weather/hourbyhour/l/San+Mateo+CA?canonicalCityId=660fecd1e7fdfd732a35b6e25c98aa50a993ec1afeac74e9b179a57eae331770';
+
+app.get('/', async (req, res) => {
+
+  const response = await fetch(weatherUrl);
+  const body = await response.text();
+  const dom = parse(body);
+  const rainChange = Number.parseInt(dom.querySelector('[data-testid="DetailsSummary"] [data-testid="PercentageValue"]').firstChild.text.replace('%', ''));
+
+  const ds = new Date().toISOString();
+  const day = new Date();
+  day.setHours(7);
+  day.setMinutes(0);
+  day.setSeconds(0);
+  day.setMilliseconds(0);
+  const dateStr = day.toISOString();
+
+  const item = rainChange > 10 ? `<item>
+  <title>Rain Alert!</title>
+  <description>${rainChange}% chance of rain today.</description>
+  <link>${weatherUrl}</link>
+  <pubDate>${dateStr}</pubDate>
+ </item>` : '';
+
+  res.setHeader('content-type', 'text/xml');
   res.send(`<?xml version="1.0" encoding="UTF-8" ?>
   <rss version="2.0">
   <channel>
-   <title>Rain Alerts</title>
-   <description>Posts on rainy days.</description>
-   <lastBuildDate>${new Date().toISOString()}</lastBuildDate>
-   <pubDate>${new Date().toISOString()}</pubDate>
+   <title>Weather Alerts</title>
+   <description>Rainy day feed.</description>
+   <lastBuildDate>${ds}</lastBuildDate>
+   <pubDate>${ds}</pubDate>
    <ttl>1800</ttl>
   
-   <item>
-    <title>Example entry</title>
-    <description>Here is some text containing an interesting description.</description>
-    <link>http://www.example.com/blog/post/1</link>
-    <guid isPermaLink="false">7bd204c6-1655-4c27-aeee-53f933c5395f</guid>
-    <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
-   </item>
+  ${item}
   
   </channel>
-  </rss>`)
+  </rss>`);
 });
 
 app.listen(port);
